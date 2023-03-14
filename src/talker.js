9dce0affeea9e8fafdbdb7439df1c52c2e769155
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-14 15:00:07 -0300

    Fix: Lint

--- src/talker.js
+++ src/talker.js
@@ -6,7 +6,7 @@ const talkerPath = path.resolve(__dirname, './talker.json');
 async function readTalkerFile() {
   try {
     const response = await fs.readFile(talkerPath, 'utf-8');
-    const data = JSON.parse(response)
+    const data = JSON.parse(response);
     return data;
   } catch (error) {
     console.log(error.message);
