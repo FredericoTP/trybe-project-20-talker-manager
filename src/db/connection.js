76816ade1896a213be9ba714d6921c7a8c2e39cc
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-03-14 21:40:42 -0300

    Fix: Req 12

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
+  host: process.env.MYSQL_HOSTNAME,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE_NAME,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
